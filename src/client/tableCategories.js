d14f7115aff6254834617a2c94adddea6533d5f0
RoinerRocha <rochabozaroiner@gmail.com>
2023-03-20 15:40:17 -0600

    eliminar categorias

--- src/client/tableCategories.js
+++ src/client/tableCategories.js
@@ -13,17 +13,37 @@ function TableCategories() {
         todasCategorias(setCategorias)
     }, [])
 
-    function LogicaCat(navigate){ 
-            navigate("/categorias");
-        
+    function LogicaCat(navigate) {
+        navigate("/categorias");
+
     }
-    const navigate=useNavigate();
+    const deleteCategory = _id => {
+        const isDelete = window.confirm(`Desea eliminar esta categoria?${_id}`)
+        if (isDelete) {
+            axios.delete(`http://localhost:5000/category/${_id}`, {
+                _id: _id
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (res) {
+                console.log(res);
+                if (res) {
+                    navigate("/home");
+                }
+            }).catch(error => {
+                console.log("error: " + error);
+                alert("NO se pudo eliminar");
+            });                 
+        }
+    }
+    const navigate = useNavigate();
     return (
         <div className="tabla">
             {<Header />}
             <Container className="tablita">
                 <br />
-                <Button onClick={()=>LogicaCat(navigate)}color="success">Insertar nueva noticia</Button>
+                <Button onClick={() => LogicaCat(navigate)} color="success">Insertar nueva categoria</Button>
                 <br /><br />
                 <Table>
                     <thead>
@@ -32,13 +52,13 @@ function TableCategories() {
                     </thead>
                     <tbody>
                         {categorias !== null ? (categorias.map(cat => (
-                           <tr>
-                           <td>{cat.name}</td>
-                           <td>
-                               <Button color="primary">editar</Button>{"  "}
-                               <Button color="danger">eliminar</Button>
-                           </td>
-                       </tr>
+                            <tr>
+                                <td>{cat.name}</td>
+                                <td>
+                                    <Button color="primary">editar</Button>{"  "}
+                                    <Button onClick={() => deleteCategory(cat._id)} color="danger">eliminar</Button>
+                                </td>
+                            </tr>
                         ))) : ('no hay cosas')}
                     </tbody>
                 </Table>
